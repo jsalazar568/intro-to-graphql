267f4c53d3646abadfce790a8bce9ac505a9f11f
Johanna Salazar Bove <johanna.salazar.b@gmail.com>
2019-08-07 00:32:29 -0400

    Building product' resolvers

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,10 +9,43 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+const products = () => {
+  return Product.find({}).exec()
+}
+
+const product = (_, args) => {
+  return Product.findById(args.id).exec()
+}
+
+const newProduct = (_, args, ctx) => {
+  const createdBy = ctx.user
+  return Product.create({...args.input, createdBy})
+}
+
+const updateProduct = (_, args) => {
+  return Product.findByIdAndUpdate(args.id, args.input, {new: true})
+}
+
+const removeProduct = (_, args) => {
+  return Product.findByIdAndDelete(args.id)
+}
+
+const createdBy = (product) => {
+  return User.findById(product.createdBy).exec()
+}
+
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    products,
+    product
+  },
+  Mutation: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {},
+    createdBy
   }
 }
